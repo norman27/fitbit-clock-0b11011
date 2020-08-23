4fe7aea5490096347a682271bffe910855d6e5bd
nmalessa <norman.malessa@trivago.com>
2020-08-23 02:05:11 +0200

    persist settings on device

--- app/index.js
+++ app/index.js
@@ -1,20 +1,16 @@
+import { me } from "appbit";
 import clock from "clock";
 import document from "document";
+import * as fs from "fs";
 import * as messaging from "messaging";
 import inverseBinaryFromNumber from "../common/utils.js";
+
+const SETTINGS_FILE = 'settings.json';
 
 clock.granularity = "seconds";
 
 // initialize settings with default colors
-let settings = {
-    "color.active": "white",
-    "color.inactive": "slategray"
-};
-
-// listen on settings changes
-messaging.peerSocket.onmessage = function(evt) {
-    settings[evt.data.key] = evt.data.value;
-}
+let settings = loadSettings();
 
 const hours = {
   1: document.getElementById("hours_1"),
@@ -63,4 +59,28 @@ clock.ontick = (evt) => {
         ? settings["color.active"]
         : settings["color.inactive"];
   }
+}
+
+// listen on settings changes
+messaging.peerSocket.onmessage = function(evt) {
+  settings[evt.data.key] = evt.data.value;
+}
+
+// register for unload event
+me.onunload = saveSettings;
+
+function loadSettings() {
+  try {
+    return fs.readFileSync(SETTINGS_FILE, {encoding: "json"});
+  } catch (exception) {
+    console.log("default settings");
+    return {
+      "color.active": "white",
+      "color.inactive": "slategray"
+    }
+  }
+}
+
+function saveSettings() {
+  fs.writeFileSync(SETTINGS_FILE, settings)
 }
